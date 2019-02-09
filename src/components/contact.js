0d9bb173787215f5e71e6f73230a3bb3f55d5d22
Gabriel Leme <gabriel.leme@digisystem.com.br>
2019-02-09 21:21:38 -0200

    Contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell } from 'react-mdl';
+import { List, ListItem, ListItemContent } from 'react-mdl';
 class Contact extends Component {
     render() {
-        return(
-            <div><h1>Contact Page</h1></div>
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Gabriel Leme</h2>
+                        <img
+                            src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt="avatar"
+                            style={{ height: '250px' }}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting
+                            industry. Lorem Ipsum has been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a galley of type
+                             and scrambled it to make a type specimen book.</p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-phone-square" aria-hidden="true" />
+                                        (55) 11-9-5315-7683
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-envelope" aria-hidden="true" />
+                                        gabriel.chimenezleme@gmail.com 
+                                    </ListItemContent>
+                                </ListItem>
+
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
     render() {
         return(
-            <div><h1>Resume Page</h1></div>
+            <div>
+                <Grid>
+                    <Cell className="resume-left-col" col={4}>left side</Cell>
+                    <Cell className="resume-right-col" col={8}>right side</Cell>
+                </Grid>
+            </div>
         )
     }
 }
